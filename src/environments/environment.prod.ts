549227786c6042200202c0e3a12cb1d26218e910
nitesh-kumar-gupta <nit90esh@gmail.com>
2019-09-21 12:10:01 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  API: 'http://localhost:3000/api/v1',
+  APP_EXTENSION: 'localhost',
+  SECRET: 'c5796a2ae795a3f3bb3c9d13b814ad357957c19c4e0637abe39f9aefb7e92940',
+  GreCaptchaV3SiteKey: '6LeoJ7YUAAAAADLgY03NQa1avISn2rLkKfEEwW8t'
 };
